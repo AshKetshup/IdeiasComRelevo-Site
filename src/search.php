30e0246319ece6a538f157d871850f15f8e46fcc
Diogo Simões <dsimoes2000@gmail.com>
2022-02-23 17:40:54 +0000

    Finishes Search.php

--- src/search.php
+++ src/search.php
@@ -58,13 +58,26 @@
             <h3>Filtrar</h3>
             <form onsubmit="event.preventDefault();" role="filter">
                 <!-- Moradia / Apartamento -->
+                <div class="accordion-filter" open="true">
+                    <div class="accordion-content">
+                        <!-- Tipo de venda (fixo) -->
+                        <div>
+                            <input type="checkbox" name="Venda">
+                            <label for="Venda">Venda</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="Alugar">
+                            <label for="Alugar">Alugar</label>
+                        </div>
+                    </div>
+                </div>
                 <div class="accordion-filter" open="false">
                     <button onclick="open_accordion(this)">
                         Tipo
                         <ion-icon name="add-outline"></ion-icon>
                     </button>
                     <div class="accordion-content">
-                        <!-- <p>Dumb Shit saying here</p> -->
+                        <!-- Tipo (fixo) -->
                         <div>
                             <input type="checkbox" name="Moradia">
                             <label for="Moradia">Moradia</label>
@@ -75,28 +88,223 @@
                         </div>
                     </div>
                 </div>
-                <!-- Alugar/Venda -->
                 <div class="accordion-filter" open="false">
                     <button onclick="open_accordion(this)">
-                        Tipo de Venda
+                        Tipologia
                         <ion-icon name="add-outline"></ion-icon>
                     </button>
                     <div class="accordion-content">
-                        <!-- <p>Dumb Shit saying here</p> -->
+                        <!-- Carregar dinamicamente segundo os disponiveis -->
                         <div>
-                            <input type="checkbox" name="Moradia">
-                            <label for="Moradia">Moradia</label>
+                            <input type="checkbox" name="T0">
+                            <label for="T0">T0</label>
                         </div>
                         <div>
-                            <input type="checkbox" name="Apartamento">
-                            <label for="Apartamemto">Apartamento</label>
+                            <input type="checkbox" name="T1">
+                            <label for="T1">T1</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="T2">
+                            <label for="T2">T2</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="T3">
+                            <label for="T3">T3</label>
                         </div>
                     </div>
                 </div>
+                <div class="accordion-filter" open="false">
+                    <button onclick="open_accordion(this)">
+                        Nº Pisos
+                        <ion-icon name="add-outline"></ion-icon>
+                    </button>
+                    <div class="accordion-content">
+                        <!-- Carregar dinamicamente segundo os disponiveis -->
+                        <div>
+                            <input type="checkbox" name="0">
+                            <label for="0">0</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="1">
+                            <label for="1">1</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="2">
+                            <label for="2">2</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="3">
+                            <label for="3">3</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-filter" open="false">
+                    <button onclick="open_accordion(this)">
+                        Piso
+                        <ion-icon name="add-outline"></ion-icon>
+                    </button>
+                    <div class="accordion-content">
+                        <!-- Carregar dinamicamente segundo os disponiveis -->
+                        <div>
+                            <select name="Andar" id="andar" default="0">
+                                <option value="0">---</option>
+                                <option value="1">Rés-de-Chão</option>
+                                <option value="2">1</option>
+                                <option value="3">2</option>
+                                <option value="4">3</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>                
+                <div class="accordion-filter" open="false">
+                    <button onclick="open_accordion(this)">
+                        Garagem
+                        <ion-icon name="add-outline"></ion-icon>
+                    </button>
+                    <div class="accordion-content">
+                        <!-- Garagem (fixo) -->
+                        <div>
+                            <input type="checkbox" name="sim_garagem">
+                            <label for="sim_garagem">Sim</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="nao_garagem">
+                            <label for="nao_garagem">Não</label>
+                        </div>
+                    </div>
+                </div>
+
+                <button type="submit">Aplicar</button>
             </form>
         </div>
         <div class="shelf-wrapper">
             <div class="shelf">
+                <a href="" class="item" visible="true">
+                    <div class="popup" title="Ultima unidade">!</div>
+                    <div class="wrapper">
+                        <img src="./assets/img/dummie/d-moradia.jpg" alt="">
+                        <div class="details">
+                            <h4>Moradia T4</h4>
+                            <h5>Entroncamento - Casal do Grilo</h5>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <div>
+                            <div class="from" visible="true">
+                                <p>A partir de</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="show">
+                        <p id="sell">Venda</p>
+                        <p id="amount">200.000€</p>
+                    </div>
+                </a>
+                <a href="" class="item" visible="true">
+                    <div class="popup" title="Ultima unidade">!</div>
+                    <div class="wrapper">
+                        <img src="./assets/img/dummie/d-moradia.jpg" alt="">
+                        <div class="details">
+                            <h4>Moradia T4</h4>
+                            <h5>Entroncamento - Casal do Grilo</h5>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <div>
+                            <div class="from" visible="true">
+                                <p>A partir de</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="show">
+                        <p id="sell">Venda</p>
+                        <p id="amount">200.000€</p>
+                    </div>
+                </a>
+                <a href="" class="item" visible="true">
+                    <div class="popup" title="Ultima unidade">!</div>
+                    <div class="wrapper">
+                        <img src="./assets/img/dummie/d-moradia.jpg" alt="">
+                        <div class="details">
+                            <h4>Moradia T4</h4>
+                            <h5>Entroncamento - Casal do Grilo</h5>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <div>
+                            <div class="from" visible="true">
+                                <p>A partir de</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="show">
+                        <p id="sell">Venda</p>
+                        <p id="amount">200.000€</p>
+                    </div>
+                </a>
+                <a href="" class="item" visible="true">
+                    <div class="popup" title="Ultima unidade">!</div>
+                    <div class="wrapper">
+                        <img src="./assets/img/dummie/d-moradia.jpg" alt="">
+                        <div class="details">
+                            <h4>Moradia T4</h4>
+                            <h5>Entroncamento - Casal do Grilo</h5>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <div>
+                            <div class="from" visible="true">
+                                <p>A partir de</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="show">
+                        <p id="sell">Venda</p>
+                        <p id="amount">200.000€</p>
+                    </div>
+                </a>
+                <a href="" class="item" visible="true">
+                    <div class="popup" title="Ultima unidade">!</div>
+                    <div class="wrapper">
+                        <img src="./assets/img/dummie/d-moradia.jpg" alt="">
+                        <div class="details">
+                            <h4>Moradia T4</h4>
+                            <h5>Entroncamento - Casal do Grilo</h5>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <div>
+                            <div class="from" visible="true">
+                                <p>A partir de</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="show">
+                        <p id="sell">Venda</p>
+                        <p id="amount">200.000€</p>
+                    </div>
+                </a>
+                <a href="" class="item" visible="true">
+                    <div class="popup" title="Ultima unidade">!</div>
+                    <div class="wrapper">
+                        <img src="./assets/img/dummie/d-moradia.jpg" alt="">
+                        <div class="details">
+                            <h4>Moradia T4</h4>
+                            <h5>Entroncamento - Casal do Grilo</h5>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <div>
+                            <div class="from" visible="true">
+                                <p>A partir de</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="show">
+                        <p id="sell">Venda</p>
+                        <p id="amount">200.000€</p>
+                    </div>
+                </a>
                 <a href="" class="item" visible="true">
                     <div class="popup" title="Ultima unidade">!</div>
                     <div class="wrapper">

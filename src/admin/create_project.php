b03f3a67a52332e92ff49d2dc938a4cadf84c165
Pedro Cavaleiro <p.cavaleiro@icloud.com>
2022-08-29 15:29:59 +0100

    Head and Scripts includes

--- src/admin/create_project.php
+++ src/admin/create_project.php
@@ -1,27 +1,14 @@
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
 <html lang="pt">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Criar Projeto | Ideias com Relevo</title>
-
-    <!-- <? $_SERVER['DOCUMENT_ROOT']; ?> -->
-
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome Icons -->
-    <script src="https://kit.fontawesome.com/539eca1b92.js" crossorigin="anonymous"></script>
-
-    <link rel="stylesheet" href="./plugins/fontawesome-free/css/all.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="./dist/css/adminlte.min.css">
-</head>
+<!-- 
+    This contains the head part of the file.
+    To set the page name change the $PAGE_NAME variable
+-->
+<?php 
+    $PAGE_NAME = "Criar Projeto";
+    include_once '../includes/admin/head.php'; 
+?>
 <style>
     input::-webkit-outer-spin-button,
     input::-webkit-inner-spin-button {
@@ -528,18 +515,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
         <!-- Main Footer -->
         <?php include_once '../includes/admin/footer.php'; ?>
-        
+
     </div>
     <!-- ./wrapper -->
 
     <!-- REQUIRED SCRIPTS -->
-
-    <!-- jQuery -->
-    <script src="./plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="./plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="./dist/js/adminlte.min.js"></script>
+    <?php include_once '../includes/admin/head.php'; ?>
+    
     <script>
         class Tipologia {
             #area = 0;
--- src/admin/projetos.php
+++ src/admin/projetos.php
@@ -1,23 +1,15 @@
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
 <html lang="pt">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Criar Projeto | Ideias com Relevo</title>
+<!-- 
+    This contains the head part of the file.
+    To set the page name change the $PAGE_NAME variable
+-->
+<?php 
+    $PAGE_NAME = "Projetos";
+    include_once '../includes/admin/head.php'; 
+?>
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="./plugins/fontawesome-free/css/all.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="./dist/css/adminlte.min.css">
-</head>
 <style>
     input::-webkit-outer-spin-button,
     input::-webkit-inner-spin-button {
@@ -269,19 +261,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
         <!-- Main Footer -->
         <?php include_once '../includes/admin/footer.php'; ?>
-        
+
     </div>
     <!-- ./wrapper -->
 
     <!-- REQUIRED SCRIPTS -->
+    <?php include_once '../includes/admin/head.php'; ?>
 
-    <!-- jQuery -->
-    <script src="./plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="./plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="./dist/js/adminlte.min.js"></script>
-    <script src="https://kit.fontawesome.com/539eca1b92.js" crossorigin="anonymous"></script>
 </body>
 
 </html>

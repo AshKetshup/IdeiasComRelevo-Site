22a27122ce3a2f3fd218ed8f95a37c08c77be5d6
Diogo Simões <dsimoes2000@gmail.com>
2022-08-29 20:26:07 +0100

    Adds logout button & Tweaks navbar

--- src/includes/admin/navbar.php
+++ src/includes/admin/navbar.php
@@ -8,12 +8,18 @@
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
-        <li class="nav-item align-self-center">
+        <li class="nav-item align-self-center mx-3">
             Username
         </li>
+        
         <li class="nav-item">
             <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                 <i class="fas fa-expand-arrows-alt"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/backend/post_scripts/logout.php" role="button">
+                <i class="fas fa-sign-out-alt"></i>
             </a>
         </li>
     </ul>

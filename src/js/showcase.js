417172739c5c433e01b5d60a15a0ddf10916e948
Pedro Cavaleiro <p.cavaleiro@icloud.com>
2022-08-29 14:22:06 +0100

    Deleted front end code
    The frontend was completely rewritten, all code was deleted

--- src/js/showcase.js
+++
@@ -1,7 +0,0 @@
-function load() {
-    var base = document.getElementsByClassName("array").firstChild;
-
-    document.querySelector(".highlight > img").setAttribute("src", base.getAttribute("src"));
-}
-
-

56ac64d4c0e59b34bce35b4029ff25fcc21b85a8
Pedro Cavaleiro <p.cavaleiro@icloud.com>
2022-08-30 12:54:39 +0100

    Update create_project.php

--- src/backend/post_scripts/create_project.php
+++ src/backend/post_scripts/create_project.php
@@ -3,6 +3,12 @@
     require_once $_SERVER["DOCUMENT_ROOT"] . '/backend/app.php';
     $app_instance = new IdeiasComRelevo();
 
-    var_dump($_POST);
+    // Takes raw data from the request
+    $json = file_get_contents('php://input');
+
+    // Converts it into a PHP object
+    $data = json_decode($json);
+
+    var_dump($data);
 
 ?>

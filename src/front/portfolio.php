0c8031e914e812636a87bbf46bd321d9287ef502
Pedro Cavaleiro <p.cavaleiro@icloud.com>
2022-08-30 02:12:12 +0100

    Adds IdeiasComRelevo php app to all files

--- src/front/portfolio.php
+++ src/front/portfolio.php
@@ -1,3 +1,7 @@
+<?php
+    require_once $_SERVER["DOCUMENT_ROOT"] . '/backend/app.php';
+    $app_instance = new IdeiasComRelevo();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,46 +17,11 @@
 
 <body class="index-page sidebar-collapse">
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg bg-primary fixed-top">
-        <div class="container">
-            <div class="navbar-translate m-0 p-0 col-3">
-                <a class="navbar-brand w-100 h-100" href="index" rel="tooltip" data-placement="bottom">
-                    <!-- Ideias Com Relevo -->
-                    <img src="./assets/img/Brand/white_logotype.png" alt="logo_brand">
-                </a>
-                <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navigation" aria-controls="navigation-index" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-bar top-bar"></span>
-                    <span class="navbar-toggler-bar middle-bar"></span>
-                    <span class="navbar-toggler-bar bottom-bar"></span>
-                </button>
-            </div>
-            <div class="col-9 collapse navbar-collapse justify-content-end m-0 p-0" id="navigation"
-                data-nav-image="./assets/img/blurred-image-1.jpg">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="imoveis">
-                            <i class="now-ui-icons design_app"></i>
-                            <p>Imóveis</p>
-                        </a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a href="portfolio" class="nav-link">
-                            <i class="now-ui-icons design_app"></i>
-                            <p>Portfolio</p>
-                        </a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a href="#footer" class="nav-link">
-                            <i class="now-ui-icons design_app"></i>
-                            <p>Contactos</p>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php 
+        $PAGE_ID = "portfolio";
+        include_once $_SERVER["DOCUMENT_ROOT"] . '/includes/site/nav.php'; 
+    ?>
+
     <!-- End Navbar -->
     <div class="container vh-100 d-flex justify-content-start align-items-center">
         <form id="searchAccordion" class="w-100 card-collapse" aria-multiselectable="true" action="">
--- src/front/produto.php
+++ src/front/produto.php
@@ -1,3 +1,7 @@
+<?php
+    require_once $_SERVER["DOCUMENT_ROOT"] . '/backend/app.php';
+    $app_instance = new IdeiasComRelevo();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,46 +17,11 @@
 
 <body class="index-page sidebar-collapse">
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg bg-primary fixed-top">
-        <div class="container">
-            <div class="navbar-translate m-0 p-0 col-3">
-                <a class="navbar-brand w-100 h-100" href="index" rel="tooltip" data-placement="bottom">
-                    <!-- Ideias Com Relevo -->
-                    <img src="./assets/img/Brand/white_logotype.png" alt="logo_brand">
-                </a>
-                <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navigation" aria-controls="navigation-index" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-bar top-bar"></span>
-                    <span class="navbar-toggler-bar middle-bar"></span>
-                    <span class="navbar-toggler-bar bottom-bar"></span>
-                </button>
-            </div>
-            <div class="col-9 collapse navbar-collapse justify-content-end m-0 p-0" id="navigation"
-                data-nav-image="./assets/img/blurred-image-1.jpg">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="imoveis">
-                            <i class="now-ui-icons design_app"></i>
-                            <p>Imóveis</p>
-                        </a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a href="portfolio" class="nav-link">
-                            <i class="now-ui-icons design_app"></i>
-                            <p>Portfolio</p>
-                        </a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a href="#footer" class="nav-link">
-                            <i class="now-ui-icons design_app"></i>
-                            <p>Contactos</p>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php 
+        $PAGE_ID = "produto";
+        include_once $_SERVER["DOCUMENT_ROOT"] . '/includes/site/nav.php'; 
+    ?>
+
     <!-- End Navbar -->
 
     <div class="container d-flex justify-content-center mb-2 flex-column" style="margin-top: 90px;">

75f4d6e96f398366ed867707fdcba7586ef1a594
Diogo Simões <dsimoes2000@gmail.com>
2022-09-04 00:43:02 +0100

    Adds php injection to product using ID from URL

--- src/front/produto.php
+++ src/front/produto.php
@@ -1,8 +1,8 @@
 <?php
-    require_once $_SERVER["DOCUMENT_ROOT"] . '/backend/app.php';
-    $app_instance = new IdeiasComRelevo();
+require_once $_SERVER["DOCUMENT_ROOT"] . '/backend/app.php';
+$app_instance = new IdeiasComRelevo();
 
-    $project = $app_instance->ProjectsManagement->get_project($_GET['id']);
+$project = $app_instance->ProjectsManagement->get_project($_GET['id']);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,9 +19,9 @@
 
 <body class="index-page sidebar-collapse">
     <!-- Navbar -->
-    <?php 
-        $PAGE_ID = "produto";
-        include_once $_SERVER["DOCUMENT_ROOT"] . '/includes/site/nav.php'; 
+    <?php
+    $PAGE_ID = "produto";
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/includes/site/nav.php';
     ?>
 
     <!-- End Navbar -->
@@ -29,28 +29,27 @@
     <div class="container d-flex justify-content-center mb-2 flex-column" style="margin-top: 90px;">
         <div id="carousel-product" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
-                <?php for ($i=0; $i < count($project->get_photos()); $i++) { 
+                <?php for ($i = 0; $i < count($project->get_photos()); $i++) {
                     if ($i == 0) {
-                        echo '<li data-target="#carousel-product" data-slide-to="'.$i.'" class="active"></li>';
+                        echo '<li data-target="#carousel-product" data-slide-to="' . $i . '" class="active"></li>';
                         continue;
                     }
 
-                    echo '<li data-target="#carousel-product" data-slide-to="'.$i.'"></li>';
+                    echo '<li data-target="#carousel-product" data-slide-to="' . $i . '"></li>';
                 } ?>
             </ol>
             <div class="carousel-inner" style="height: 500px" role="listbox">
-                <?php 
-                    $x = 0;
-                    foreach ($project->get_photos() as $photo) {
-                        
-                        echo (
-                            ($x == 0) 
-                                ? '<div class="carousel-item active">'
-                                : '<div class="carousel-item">'
-                        ).'<img class="d-block" src="/uploads/'.$photo.'"></div>';
-                    
-                        $x++;
-                    } 
+                <?php
+                $x = 0;
+                foreach ($project->get_photos() as $photo) {
+                    echo (
+                        ($x == 0)
+                        ? '<div class="carousel-item active">'
+                        : '<div class="carousel-item h-100 w-100">'
+                    ) . '<img class="d-block" src="/uploads/' . $photo . '" style="width: 100%; min-height: 100%; vertical-align: middle;"></div>';
+
+                    $x++;
+                }
                 ?>
             </div>
             <a class="carousel-control-prev" href="#carousel-product" role="button" data-slide="prev">
@@ -68,25 +67,43 @@
         <!-- Nav tabs -->
         <ul class="nav nav-tabs nav-fill m-0 p-0 mt-3 mb-2" id="myTab" role="tablist">
             <li class="nav-item">
-                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#descrição" role="tab" aria-controls="descrição"
-                    aria-selected="true">Descrição</a>
+                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#descrição" role="tab" aria-controls="descrição" aria-selected="true">Descrição</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" id="pisos-tab" data-toggle="tab" href="#pisos" role="tab"
-                    aria-controls="pisos" aria-selected="false">Pisos</a>
+                <a class="nav-link" id="pisos-tab" data-toggle="tab" href="#pisos" role="tab" aria-controls="pisos" aria-selected="false">Pisos</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" id="info-tab" data-toggle="tab" href="#info" role="tab"
-                    aria-controls="info" aria-selected="false">Informação</a>
+                <a class="nav-link" id="info-tab" data-toggle="tab" href="#info" role="tab" aria-controls="info" aria-selected="false">Informação</a>
             </li>
         </ul>
 
         <!-- Tab panes -->
         <div class="tab-content">
-            <div class="tab-pane active" id="descrição" role="tabpanel" aria-labelledby="descrição-tab" style="height: 50vh;">
+            <div class="tab-pane active" id="descrição" role="tabpanel" aria-labelledby="descrição-tab" style="min-height: 50vh;">
                 <?= $project->get_description() ?>
             </div>
-            <div class="tab-pane" id="pisos" role="tabpanel" aria-labelledby="pisos-tab" style="height: 50vh;">....</div>
+            <div class="tab-pane" id="pisos" role="tabpanel" aria-labelledby="pisos-tab" style="height: 50vh;">
+                <div id="accordion" role="tablist" aria-multiselectable="true" class="card-collapse">
+                    <div class="card">
+                        <div class="card-header" role="tab" id="headingOne">
+                            <button class="btn btn-link btn-block text-left h3 my-0 py-0 " data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Collapsible Group Item #1
+
+                                <i class="now-ui-icons arrows-1_minimal-down"></i>
+                            </button>
+                        </div>
+
+                        <div id="collapseOne" class="collapse show" role="tabpanel" aria-labelledby="headingOne">
+                            <div class="card-body">
+                                <h3 class="col-12">Detalhes</h3>
+                                <div class="col-6">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="tab-pane" id="info" role="tabpanel" aria-labelledby="info-tab" style="height: 50vh;">.....</div>
         </div>
 
@@ -103,76 +120,18 @@
         </style>
     </div>
 
-    <!-- Sart Modal -->
-    <!-- <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header justify-content-center">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
-                            <i class="now-ui-icons ui-1_simple-remove"></i>
-                        </button>
-                        <h4 class="title title-up">Modal title</h4>
-                    </div>
-                    <div class="modal-body">
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
-                            there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the
-                            Semantics, a large language ocean. A small river named Duden flows by their place and
-                            supplies it with the necessary regelialia. It is a paradisematic country, in which roasted
-                            parts of sentences fly into your mouth.
-                        </p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default">Nice Button</button>
-                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-    <!--  End Modal -->
-    <!-- Mini Modal -->
-    <!-- <div class="modal fade modal-mini modal-primary" id="myModal1" tabindex="-1" role="dialog"
-            aria-labelledby="myModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header justify-content-center">
-                        <div class="modal-profile">
-                            <i class="now-ui-icons users_circle-08"></i>
-                        </div>
-                    </div>
-                    <div class="modal-body">
-                        <p>Always have an access to your profile</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-link btn-neutral">Back</button>
-                        <button type="button" class="btn btn-link btn-neutral" data-dismiss="modal">Close</button>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-    <!--  End Modal -->
-    
     <!-- footer -->
     <?php include_once $_SERVER["DOCUMENT_ROOT"] . '/includes/site/footer.php'; ?>
-    
+
     <!-- scripts -->
     <?php include_once $_SERVER["DOCUMENT_ROOT"] . '/includes/site/scripts.php'; ?>
+    <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
+    <script>
+        let desc = document.getElementById("descrição");
 
-    <!-- <script>
-        $(document).ready(function () {
-            // the body of this function is in assets/js/now-ui-kit.js
-            nowuiKit.initSliders();
-        });
+        desc.innerHTML = marked.parse(desc.innerText);
+    </script>
 
-        function scrollToDownload() {
-
-            if ($('.section-download').length != 0) {
-                $("html, body").animate({
-                    scrollTop: $('.section-download').offset().top
-                }, 1000);
-            }
-        }
-    </script> -->
 </body>
 
 </html>
